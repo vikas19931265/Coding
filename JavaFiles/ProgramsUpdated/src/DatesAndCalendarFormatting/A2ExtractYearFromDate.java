41e86133854938558250950ded85f01984be190b
vikas12651993 <vikas1265@hotmail.com>
2021-04-24 18:38:25 +0530

    updates files

--- JavaFiles/ProgramsUpdated/src/DatesAndCalendarFormatting/A2ExtractYearFromDate.java
+++ JavaFiles/ProgramsUpdated/src/DatesAndCalendarFormatting/A2ExtractYearFromDate.java
@@ -7,8 +7,11 @@ class A2ExtractYearFromDate
 {
     public static void main(String[] args) throws ParseException
     {
-        String date="06/09/1993";
-        Date format= new SimpleDateFormat("dd/MM/yyyy").parse(date);
+        String date="06-09-1993";
+        SimpleDateFormat formats= new SimpleDateFormat("yyyy-MM-dd");  //// this will not throw any exception..to format against specific format
+        formats.setLenient(false);
+        Date format =formats.parse(date);
+        System.out.println(format);
         Calendar c =Calendar.getInstance();
         c.setTime(format);
         int year=c.get(Calendar.YEAR);
@@ -18,6 +21,17 @@ class A2ExtractYearFromDate
     }
 }
 /*
+If date is not right format we will get the exception as 
+
+run:
+Exception in thread "main" java.text.ParseException: Unparseable date: "06-09-1993"
+	at java.text.DateFormat.parse(Unknown Source)
+	at DatesAndCalendarFormatting.A2ExtractYearFromDate.main(A2ExtractYearFromDate.java:13)
+C:\Users\Vikas.Gautam\AppData\Local\NetBeans\Cache\8.2\executor-snippets\run.xml:53: Java returned: 1
+BUILD FAILED (total time: 2 seconds)
+
+otherwise
+
 run:
 1993
 BUILD SUCCESSFUL (total time: 0 seconds)
